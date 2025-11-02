e751cb20eb8d10d7dccc00d36ddeb17d97c6d111
Vincent Winberg <winbergvincent@gmail.com>
2025-11-02 21:41:57 +0100

    disabled conform for real (this time)

--- neovim/.config/nvim/lua/plugins/conform.lua
+++ neovim/.config/nvim/lua/plugins/conform.lua
@@ -19,12 +19,12 @@ return {
                 --lua = { "lua-format" },
             },
 
-            format_on_save = {
-                -- These options will be passed to conform.format()
-                timeout_ms = 500,
-                fallback = true,
-                lsp_format = "fallback",
-            },
+            -- format_on_save = {
+            --    -- These options will be passed to conform.format()
+            --    timeout_ms = 500,
+            --    fallback = true,
+            --    lsp_format = "fallback",
+            --},
         })
     end,
 }
